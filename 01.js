ed2d2579df3feaad9779ecbc634e99dfa32a44ff
Jhoabe Leão <jhoabe.leao@hotmail.com>
2022-03-03 19:40:43 -0300

    Exercício 1

--- 01.js
+++ 01.js
@@ -1,6 +1,6 @@
 /*
 Crie uma função que dado dois valores (passados como parâmetros) mostre no console a 
-soma, subtração,multiplicação e divisão desses valores
+soma, subtração, multiplicação e divisão desses valores.
 */
 
 function calcularOperacao (operador1, operador2) {
